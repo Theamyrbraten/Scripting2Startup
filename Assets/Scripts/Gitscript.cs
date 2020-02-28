a8d6d35835daf269a9b5a772f0fc11c749af617b
Thea Sofie Myrbråten <themyrbr@gmail.com>
2020-02-28 14:21:03 +0100

    Script - print help
    printer "Help" ved start

--- Assets/Scripts/Gitscript.cs
+++ Assets/Scripts/Gitscript.cs
@@ -7,12 +7,12 @@ public class Gitscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("help");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
