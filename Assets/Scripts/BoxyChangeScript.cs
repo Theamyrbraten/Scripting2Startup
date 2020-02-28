bfbe80b103a5467d26cceac87035c1e848318700
Thea Sofie Myrbråten <themyrbr@gmail.com>
2020-02-28 14:18:22 +0100

    Initializes Unity
    Mulig info

---
+++ Assets/Scripts/BoxyChangeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxyChangeScript : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void OnBecameVisible()
+    {
+        print ("dafuq");
+        GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+        GetComponent<MeshRenderer>().material.color = new Color(
+            Random.value, Random.value, Random.value);
+    }
+}
---
+++ Assets/Scripts/InstantBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantBox : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Touch touch = Input.touches[0];
+        Vector3
+    }
+}
